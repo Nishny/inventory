ab7f00d64412a05309889a4e4d945215223726b4
wong <wong@mpim-bonn.mpg.de>
2022-09-22 17:47:18 +0200

    removed shape

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ df1=pd.read_csv("List1.csv")
 df2=pd.read_csv("List2.csv")
 
 df_final = pd.concat([df1, df2]).drop_duplicates(subset=['Name', 'Alter', 'Geschlecht']).reset_index(drop=True)
-print(df_final.shape)
 
 df_final2 = df_final.drop_duplicates(subset=['Name', 'Alter']).reset_index(drop=True)
 
